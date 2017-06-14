ffc36214b0874bcc4b93e33c872599a3efb726da
drworm75 <dwgames@gmail.com>
2017-06-13 21:28:20 -0500

    Returning name year and image from gb.  Radios for played and wishlist. Started assembling fb, and making calls for games.  Looking back, maybe I should have made more commits, and maybe a new branch.  But it's late.

--- javascripts/controllers/GameNewCtrl.js
+++ javascripts/controllers/GameNewCtrl.js
@@ -2,9 +2,23 @@ app.controller('GameNewCtrl', function($scope, GameFactory) {
 	$scope.returnedGames = "";
     $scope.header = "Game New";
     $scope.gameTitle = "Exciting New Pac-Man Plus";
-    $scope.gamesArray
+    $scope.gamesArray = [];
 
-   	$scope.searchGiantBomb = (searchString) => {
+ 	$scope.radio = () => {
+ 		console.log("radio");
+ 	};
+
+     $scope.card = (passedName) => {
+    	$scope.gamesArray.forEach((gameObj) => {
+    		if(gameObj.name === passedName) {
+    			$scope.cardName = gameObj.name;
+    			$scope.cardYear = gameObj.year;
+    			$scope.cardImage = gameObj.image;
+    		}
+    	});
+    };
+
+    $scope.searchGiantBomb = (searchString) => {
    		GameFactory.gbSearchGiantBomb(searchString).then((gbGames) => {
    			$scope.gamesArray = gbGames;
    		}).catch((error) => {
--- javascripts/factories/GameFactory.js
+++ javascripts/factories/GameFactory.js
@@ -1,18 +1,45 @@
-app.factory("GameFactory", function($http, $q, GIANTBOMB_CONFIG) {
+app.factory("GameFactory", function($http, $q, $sce, GIANTBOMB_CONFIG, FIREBASE_CONFIG) {
+
+	let fbGetGameList = () => {
+		let gamesFromFb = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/games.json`)
+			.then((fbGames) => {
+				let fbGameCollection = fbGames.data;
+				console.log(fbGameCollection);
+				Object.keys(fbGameCollection).forEach((key) => {
+					gamesFromFb.push({
+							"name": fbGameCollection[key].title,
+							"year": fbGameCollection[key].release_year
+						});
+				});
+				console.log(gamesFromFb[0].name);
+				resolve(gamesFromFb);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+		});
+	};
 	
 	let gbSearchGiantBomb = (search) => {
 		let gameNames = [];
-		console.log("search", search);
-		console.log("gbapikey is ...", GIANTBOMB_CONFIG.gbapikey);
 		return $q((resolve, reject) => {
-			$http.get(`https://www.giantbomb.com/api/search/?api_key=${GIANTBOMB_CONFIG.gbapikey}&format=json&query="${search}"&resources=game&field_list=name`)
+			$http.get(`https://www.giantbomb.com/api/search/?api_key=${GIANTBOMB_CONFIG.gbapikey}&format=json&query="${search}"&resources=game&field_list=name,image,images,original_release_date`)
 			.then((gbInfo) => {
-				console.log("GameInfo back from API", gbInfo.data.results);
-				let gameCollection = gbInfo.data.results
+				let gameCollection = gbInfo.data.results;			
 				Object.keys(gameCollection).forEach((key) => {
-					 gameNames.push(gameCollection[key].name);
-				})
-					 console.log("gameNames", gameNames)
+						if (gameCollection[key].image !== null && gameCollection[key].original_release_date !== null) {
+								gameCollection[key].image.small_url = $sce.trustAsResourceUrl(gameCollection[key].image.small_url);
+								gameCollection[key].original_release_date = gameCollection[key].original_release_date.slice(0,4);
+						gameNames.push({
+							"name": gameCollection[key].name,
+							"image": gameCollection[key].image.small_url,
+							"year": gameCollection[key].original_release_date
+						});
+					}
+				});
+					 console.log("gameNames", gameNames);
 				resolve(gameNames);
 			})
 			.catch((error) => {
@@ -23,6 +50,7 @@ app.factory("GameFactory", function($http, $q, GIANTBOMB_CONFIG) {
 
 	return {
 
-		gbSearchGiantBomb: gbSearchGiantBomb
+		gbSearchGiantBomb: gbSearchGiantBomb,
+		fbGetGameList: fbGetGameList
 	};
 });
